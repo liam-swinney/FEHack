378a01dc8d3e3acdc1457ba4d070183c422c8808
liam-swinney <liam.swinney@bedegaming.com>
2015-04-24 16:56:19 +0100

    simple backbone view

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 var Video = Backbone.Model.extend({});
 
 var VideoView = Backbone.View.extend({
-    template: '#templateVideo'
+    template: '#templateVideo',
+    initialize: function() {
+        this.render();
+        this.listenTo(this.model, "change", this.render);
+    },
+    render: function () {
+        var template = _.template($('#templateVideo').html());
+        var videoTemplate = template(this.model.attributes);
+
+        this.$el.html(videoTemplate);
+
+        return this;
+    },
+    model: Video
 });
 
-var video = new Video({});
-var videoView = VideoView({});
+var video = new Video;
+var videoView = new VideoView({model : video});
 
-// $('#videoList').append(viewView.render())
+var videoObj = {
+    name: 'video name',
+    url: 'https://www.test.com'
+};
+
+video.set(videoObj);
+
+videoView.render();
+
+$('#videoList').html(videoView.el);
