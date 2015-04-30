189a0f6f97a121c4dc7a74add203092414a5616f
Muz <mostofa.kamal@bedegaming.com>
2015-04-30 12:36:20 +0100

    Added new Models, Objects and Colleciton

--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
 var Video = Backbone.Model.extend({});
+var VideoCollection = Backbone.Collection.extend({});
 
 var VideoView = Backbone.View.extend({
-    template: '#templateVideo',
-    initialize: function() {
-        this.listenTo(this.model, "change", this.render);
+    initialize : function(){
+        this.listenTo(this.collection, 'add', this.render);
     },
+    template: '#templateVideo',
     render: function () {
         var template = _.template($('#templateVideo').html());
-        var videoTemplate = template(this.model.attributes);
+        var videoTemplate = template({
+            items: this.collection.toJSON()
+        });
 
         this.$el.html(videoTemplate);
 
         return this;
     },
-    model: Video
+    collection: VideoCollection
 });
 
-var video = new Video;
-var videoView = new VideoView({model : video});
+var video = new Video; // creating new model
+var video2 = new Video;
+var videoCollection = new VideoCollection; // creating new collection
+var videoView = new VideoView({collection : videoCollection}); // creating a view
+
+videoCollection.add(video); // adding model to collection
+videoCollection.add(video2);
 
 var videoObj = {
-    name: 'video name',
+    name: 'Muz',
     url: 'https://www.test.com'
 };
 
-video.set(videoObj);
+var videoObj2 = {
+    name: 'Liam',
+    url: 'https://goldengridsystem.com'
+}
 
-videoView.render();
+video.set(videoObj); // updating / setting the model attributes
+video2.set(videoObj2);
 
-$('#videoList').html(videoView.el);
+videoView.render(); // outputing to the view
+
+$('#videoList').html(videoView.el); // adding view to DOM
