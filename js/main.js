40fa50e5e144120afe6b61a0cb75d91868d9f5b9
liam-swinney <liam.swinney@bedegaming.com>
2015-05-06 09:14:14 +0100

    added marionette,  fetch youtube

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 var Video = Backbone.Model.extend({});
-var VideoCollection = Backbone.Collection.extend({});
 
-var VideoView = Backbone.View.extend({
+var VideoCollection = Backbone.Collection.extend({
+    url: function() {
+        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCHxd44vP-j7eB9F-dpnV3oKNpXDysDEds'
+
+        return url;
+    },
+    parse: function(data) {
+        var array = $.map(data.items, function(value, index) {
+            value.id = value.id.videoId
+            return [value];
+        });
+
+        return array;
+    }
+});
+
+var VideoView = Mn.View.extend({
     initialize : function(){
         this.listenTo(this.collection, 'add', this.render);
     },
@@ -19,27 +34,9 @@ var VideoView = Backbone.View.extend({
     collection: VideoCollection
 });
 
-var video = new Video; // creating new model
-var video2 = new Video;
+
 var videoCollection = new VideoCollection; // creating new collection
 var videoView = new VideoView({collection : videoCollection}); // creating a view
-
-videoCollection.add(video); // adding model to collection
-videoCollection.add(video2);
-
-var videoObj = {
-    name: 'Muz',
-    url: 'https://www.test.com'
-};
-
-var videoObj2 = {
-    name: 'Liam',
-    url: 'https://goldengridsystem.com'
-}
-
-video.set(videoObj); // updating / setting the model attributes
-video2.set(videoObj2);
-
-videoView.render(); // outputing to the view
+videoCollection.fetch();
 
 $('#videoList').html(videoView.el); // adding view to DOM
