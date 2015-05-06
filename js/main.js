e7a7b8d495f73fbb341d1e2f74d8446382a4974d
liam-swinney <liam.swinney@bedegaming.com>
2015-05-06 22:24:00 +0100

    handled no results
    updated events to search on keyup
    search for videos

--- js/main.js
+++ js/main.js
@@ -1,66 +1,92 @@
 var Video = Backbone.Model.extend({});
 
 var VideoCollection = Backbone.Collection.extend({
-    url: function() {
-        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCHxd44vP-j7eB9F-dpnV3oKNpXDysDEds'
+	url: function() {
+		var url = 'https://www.googleapis.com/youtube/v3/search?type=video&q='+ this.requestTerm +'&part=snippet&key=AIzaSyCHxd44vP-j7eB9F-dpnV3oKNpXDysDEds'
 
-        return url;
-    },
-    parse: function(data) {
-        var array = $.map(data.items, function(value, index) {
-            value.id = value.id.videoId
-            return [value];
-        });
+		return url;
+	},
+	parse: function(data) {
+		var array = $.map(data.items, function(value, index) {
+			value.id = value.id.videoId
+			return [value];
+		});
 
-        return array;
-    }
+		return array;
+	}
 });
 
 // Layout
 var Layout = Mn.LayoutView.extend({
-  template: '#templateLayout',
-    el: '.js-video-player-app',
-  // define regions
-  regions: {
-    videoList: '.js-video-list',
-    videoPlayer: '.js-video-player'
-  }
+	template: '#templateLayout',
+	el: '.js-video-player-app',
+	regions: {
+		videoList: '.js-video-list',
+		videoPlayer: '.js-video-player',
+		videoSearch: '.js-search'
+	}
 });
 
 var layout = new Layout();
 
 var VideoView = Mn.ItemView.extend({
-    template: '#templateVideo',
-    model: Video,
-    tagName: 'li'
+	template: '#templateVideo',
+	model: Video,
+	tagName: 'li'
 });
 
 var VideoPlayer = Mn.ItemView.extend({
-    template: '#templateVideoPlayer',
-    model: Video
+	template: '#templateVideoPlayer',
+	model: Video
+});
+
+var VideoSearch = Mn.ItemView.extend({
+	template: '#templateVideoSearch',
+	events: {
+		'click .js-submit-button': 'getVideos',
+		'keyup .js-input':  'getVideos'
+	},
+	getVideos: function(e) {
+		var searchTerm = this.$el.find('.js-input').val();
+
+		if (searchTerm) {
+			videoCollection.requestTerm = searchTerm;
+			videoCollection.fetch();
+		} else {
+			videoCollection.reset();
+		}
+	}
+});
+
+var EmptyVideoView = Mn.ItemView.extend({
+	template: '#templateVideoNoResults'
 });
 
 var VideosView = Marionette.CollectionView.extend({
-    el: '#videoList',
-    childView: VideoView,
-    collection: VideoCollection,
-    events: {
-        'click .video': 'showVideo'
-    },
-    showVideo: function(e) {
-        e.preventDefault();
-        var id = $(e.currentTarget).data("videoid");
+	el: '#videoList',
+	childView: VideoView,
+	emptyView: EmptyVideoView,
+	collection: VideoCollection,
+	events: {
+		'click .video': 'showVideo'
+	},
+	showVideo: function(e) {
+		e.preventDefault();
+		var id = $(e.currentTarget).data("videoid");
 
-        layout.videoPlayer.show(new VideoPlayer({
-            model: this.collection.get(id)
-        }));
-    }
+		layout.videoPlayer.show(new VideoPlayer({
+			model: this.collection.get(id)
+		}));
+	}
 });
 
 var videoCollection = new VideoCollection; // creating new collection
-videoCollection.fetch();
 
 layout.render();
 
 // Show inner view
-layout.videoList.show(new VideosView({collection : videoCollection}));
+layout.videoSearch.show(new VideoSearch());
+layout.videoList.show(new VideosView({
+	collection : videoCollection
+}));
+
